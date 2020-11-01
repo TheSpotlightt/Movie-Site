f1f3503c35802b1b171bf131e2e7a97eee6d3eee
Samuel Gobbo <samuelgobbo93@gmail.com>
2020-11-01 13:49:12 -0300

    Commit

--- src/movie-section/info-page/get-infos.js
+++ src/movie-section/info-page/get-infos.js
@@ -4,21 +4,15 @@ import InfoPage from './movies-info-page';
 export default function GetInfos(props) {
     const [moviesInfo, setMoviesInfo] = useState([]);
     const moviesInfoArray = [moviesInfo]
-    const movieInfos = props.infos;
+    const movieInfos = props.location.getMoviesInfo;
     
     useEffect(() => {
-        let isMounted = true;
-        fetch(`https://api.themoviedb.org/3/movie/268?api_key=d8007bb731f4937f50c8e7528e5c21e2&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/${movieInfos}?api_key=d8007bb731f4937f50c8e7528e5c21e2&language=en-US`)
         .then(res => res.json())
         .then(res => {
             setMoviesInfo(res);
         })
         .catch(error => console.log(error));
-
-        return () => {
-            isMounted = false;
-        }
-
     }, [movieInfos]);
 
     return (
--- src/movie-section/movie-posters/movie-posters.js
+++ src/movie-section/movie-posters/movie-posters.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import InfoPage from '../info-page/get-infos';
 
 export default function PostersResult (props) {
     const posters = props.image;
     const altTitle = props.altTitle;
-    const movieInfos = props.infos;
+    const movieID = props.infos;
     
     return (
         <div>
-            <Link to={`/movie/${movieInfos}`}>
+            <Link to={{
+                pathname: `/movie/${movieID}`,
+                getMoviesInfo: `${movieID}`
+            }}>
                 <img src={`https://image.tmdb.org/t/p/w342/${posters}`} alt={altTitle} />
             </Link>
-            <InfoPage 
-                infos={movieInfos}
-            />
         </div>  
     )
 }
